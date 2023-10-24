341034fdfaff5cc2a08e26dff42cceea9fc0a651
Cezar Souza <devrazec@gmail.com>
2023-10-24 19:38:30 +0200

    Updates

--- app/Routes/Web.php
+++ app/Routes/Web.php
@@ -3,11 +3,6 @@
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
-//App Root
-define('APP_ROOT', dirname(dirname(__FILE__)));
-define('URL_ROOT', '/');
-define('URL_SUBFOLDER', '');
-
 // Routes system
 $routes = new RouteCollection();
 $routes->add('home', new Route(constant('URL_SUBFOLDER') . '/', array('controller' => 'HomeController', 'method'=>'index'), array()));
--- public/index.php
+++ public/index.php
@@ -8,18 +8,11 @@ error_reporting(E_ALL);
 // Autoloader
 require_once __DIR__ . '../../vendor/autoload.php';
 
-//use App\Libs\Router;
-//use App\Routes\Web;
-//use App\Controllers\HomeController;
-//use App\Models\Setting;
-
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\ErrorHandler\ErrorHandler;
+use Symfony\Component\ErrorHandler\DebugClassLoader;
+Debug::enable();
 
 // Routes
 require_once '../App/Routes/Web.php';
 require_once '../App/Libs/Router.php';
-
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\ErrorHandler\ErrorHandler;
-use Symfony\Component\ErrorHandler\DebugClassLoader;
-
-Debug::enable();
